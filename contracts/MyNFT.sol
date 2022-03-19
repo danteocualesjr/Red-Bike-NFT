6df5eb1584b0d428e48bcac01279d5fe29d2a4e6
Dante O. Cuales <danteocualesjr@gmail.com>
2022-03-20 07:58:35 +0800

    Created empty contract for NFT

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -7,3 +7,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract MyNFT is ERC721, Ownable {
+    
+}
