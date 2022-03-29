8da88691ab368504f6b9f2b6790a701aa94e9c74
Dante O. Cuales <danteocualesjr@gmail.com>
2022-03-29 14:07:11 +0800

    Updated contract

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721, Ownable {
+    
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
