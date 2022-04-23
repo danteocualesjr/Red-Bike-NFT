3e8041d75211f24b010e3be1e87b8c71cbb65b98
Dante O. Cuales <danteocualesjr@gmail.com>
2022-04-23 17:20:29 +0800

    Updated contract

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -15,8 +15,9 @@ contract MyNFT is ERC721, Ownable {
     function mintNFT(address recipient, string memory tokenURI)
         public onlyOwner
         returns (uint256)
-        
+
     {
+        
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
