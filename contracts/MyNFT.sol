19f118a7a00ae269e640701216704c83555183ee
Dante O. Cuales <danteocualesjr@gmail.com>
2022-05-03 13:30:52 +0800

    Updated contract

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.7.3;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract MyNFT is ERC721, Ownable { 
     using Counters for Counters.Counter;
     
