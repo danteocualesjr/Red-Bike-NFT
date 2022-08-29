c3fd5ba2abe6f1426c1c28daa746472e9be1054e
Dante O. Cuales <danteocualesjr@gmail.com>
2022-08-29 23:20:56 +0800

    Refactored code

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.7.3;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
